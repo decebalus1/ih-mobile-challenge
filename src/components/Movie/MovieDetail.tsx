5032846ea8225daec3233c53253aa4838b062970
Marius Bologa <marius@foxbox.co>
2022-04-11 02:24:32 -0500

    feat: add details for movies

--- src/components/Movie/MovieDetail.tsx
+++ src/components/Movie/MovieDetail.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Dimensions, ScrollView, View, StyleSheet } from 'react-native';
 import { useLazyQuery } from '@apollo/client';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Card, Image, Text } from '@react-native-elements/themed';
 
 import { movieQuery } from '../../services/queries/movieQuery';
-import { Loading, Error } from '../shared';
+import { Loading, Error, Row } from '../shared';
 import { MovieStackParamList } from '../../navigation/stacks';
+import { formatDate } from '../../utils/date';
+
+const screenWidth = Dimensions.get('window').width;
 
 type MovieDetailProps = NativeStackScreenProps<
   MovieStackParamList,
@@ -29,6 +33,19 @@ export const MovieDetail = ({ navigation, route }: MovieDetailProps) => {
     });
   }, [navigation, route.params.title]);
 
+  const { film } = data || {};
+
+  const releasedDate = React.useMemo(
+    () => formatDate(film?.releaseDate),
+    [film?.releaseDate],
+  );
+
+  const producers = React.useMemo(() => {
+    if (film?.producers && film?.producers?.length) {
+      return film.producers.join(', ');
+    }
+  }, [film?.producers]);
+
   if (loading) {
     return <Loading />;
   }
@@ -36,10 +53,45 @@ export const MovieDetail = ({ navigation, route }: MovieDetailProps) => {
   if (error) {
     return <Error message={error.message} />;
   }
+  const getStringWithoutLineBreaks = (text: string) => {
+    return text.replace(/(\r\n|\n|\r)/gm, '');
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Movie Detail</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        bounces={false}
+      >
+        <Image
+          style={styles.image}
+          resizeMode="cover"
+          source={require('../../assets/movie_default.png')}
+        />
+        <View style={styles.infoContainer}>
+          {!!film?.openingCrawl && (
+            <Text>{getStringWithoutLineBreaks(film?.openingCrawl)}</Text>
+          )}
+        </View>
+        <Text h4 style={styles.header}>
+          Production
+        </Text>
+        <Card containerStyle={styles.card}>
+          {!!film?.director && (
+            <>
+              <Row leftText="Director" rightText={film.director} />
+              <Card.Divider />
+            </>
+          )}
+          {!!releasedDate && (
+            <>
+              <Row leftText="ReleaseDate" rightText={releasedDate} />
+              <Card.Divider />
+            </>
+          )}
+          {!!producers && <Row leftText="Producers" rightText={producers} />}
+        </Card>
+      </ScrollView>
     </View>
   );
 };
@@ -47,7 +99,30 @@ export const MovieDetail = ({ navigation, route }: MovieDetailProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  image: {
+    height: 230,
+    width: screenWidth,
+    left: 0,
+    right: 0,
+  },
+  infoContainer: {
+    marginTop: 10,
+    padding: 8,
+  },
+  header: {
+    marginTop: 10,
+  },
+  card: {
+    borderRadius: 10,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 5,
   },
 });
